9e88bd252c26445e625b5cd42db852601ef5770f
Đinh Khánh <dinhkhanh100805@gmail.com>
2024-08-12 23:08:55 +0700

    first commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Dinh Huynh Quoc Khanh");
+        System.out.println("Welcome to the DHKTPM19A");
+    }
+}
